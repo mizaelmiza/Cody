ae97e74678becb7df5207aa2fee08a940db70e2a
DMVMarcio <playdznvlt@gmail.com>
2019-02-10 15:24:08 -0200

    bot gaming

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,13 @@
 module.exports = async function () {
-    this.shardLog(`${this.user.tag} iniciado`)
-    this.setGame({random: true, force: true})
-    setInterval(async () => {
-        this.setGame({random: true, force: false})
-    }, 5 * 1000 * 60)
-    this.setDataStaff()
-    setInterval(() => {
+    setTimeout(async () => {
+        this.shardLog(`${this.user.tag} iniciado`)
+        this.setGame({random: true, force: true})
+        setInterval(async () => {
+            this.setGame({random: true, force: false})
+        }, 5 * 1000 * 60)
         this.setDataStaff()
-    }, 15 * 1000 * 60)
+        setInterval(() => {
+            this.setDataStaff()
+        }, 15 * 1000 * 60)
+    },10 * 1000)
 }
