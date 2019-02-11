a6d2445a97e5d21c12bd920b17849f56daeb7d76
DMVMarcio <playdznvlt@gmail.com>
2019-02-10 23:02:24 -0200

    lang help

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,10 @@ module.exports = async function () {
     setTimeout(async () => {
         this.shardLog(`${this.user.tag} iniciado`)
         this.setGame({random: true, force: true})
+        this.dbl.postStats(this.guilds.size, this.shards.Id, this.shards.total);
+        setInterval(() => {
+            this.dbl.postStats(client.guilds.size, client.shards.Id, client.shards.total);
+        }, 1800000);
         setInterval(async () => {
             this.setGame({random: true, force: false})
         }, 5 * 1000 * 60)
